9d6b7096284e038d95816cd2380cbb49f39ad2b5
Jorge Juan <hi.jorgejuanp@gmail.com>
2015-11-06 20:30:27 +0100

    Add initial files

---
+++ lexiconomitron.rb
@@ -0,0 +1,5 @@
+class Lexiconomitron
+  def eat_t(input)
+    input.downcase.delete "t"
+  end
+end
---
+++ spec/lexiconomitron_spec.rb
@@ -0,0 +1,11 @@
+require_relative "../lexiconomitron"
+require "spec_helper"
+
+describe Lexiconomitron do
+  describe "#eat_t" do
+    it "removes every letter t from the input" do
+      @lexi = Lexiconomitron.new
+      expect(@lexi.eat_t("great scott!")).to eq("grea sco!")
+    end
+  end
+end
