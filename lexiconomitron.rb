d055dfea474a65958e1fb8ad90b6262132d21a24
Jorge Juan <hi.jorgejuanp@gmail.com>
2015-11-06 21:08:02 +0100

    Add 'oompa_loompa' test and method

--- lexiconomitron.rb
+++ lexiconomitron.rb
@@ -2,4 +2,15 @@ class Lexiconomitron
   def eat_t(input)
     input.downcase.delete "t"
   end
+
+  def shazam(input)
+    output = input.map do |word|
+      eat_t(word.reverse)
+    end
+    [output.first, output.last]
+  end
+
+  def oompa_loompa(input)
+    input.delete_if {|word| word.length > 3}
+  end
 end
--- spec/lexiconomitron_spec.rb
+++ spec/lexiconomitron_spec.rb
@@ -2,10 +2,25 @@ require_relative "../lexiconomitron"
 require "spec_helper"
 
 describe Lexiconomitron do
+  before :each do
+    @lexi = Lexiconomitron.new
+  end
+
   describe "#eat_t" do
     it "removes every letter t from the input" do
-      @lexi = Lexiconomitron.new
       expect(@lexi.eat_t("great scott!")).to eq("grea sco!")
+    end
+  end
+
+  describe "#shazam" do
+    it "returns the first and last words in an array, reversed" do
+      expect(@lexi.shazam(["This", "is", "a", "boring", "test"])).to eq(["sih", "se"])
+    end
+  end
+
+  describe "#oompa_loompa" do
+    it "returns an array, but only words with 3 characters or less, in the same order" do
+      expect(@lexi.oompa_loompa( ["if", "you", "wanna", "be", "my", "lover"])).to eq(["if", "you", "be", "my"])
     end
   end
 end
